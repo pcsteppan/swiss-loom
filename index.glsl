e49bb6f0c1f7299e063c34eb1ad5b53830a589a2
Patrick Steppan <patrick.steppan@gmail.com>
2024-03-09 18:12:52 -0600

    tweaking colors, browser rendering size, updated swissgl

--- index.glsl
+++ index.glsl
@@ -1,8 +1,8 @@
 float xPartition = 0.92;
 float yPartition = 0.92;
 
-vec4 blue = vec4(0., 0., 1., 1.);
-vec4 gray = vec4(.7);
+vec4 black = vec4(0., 0., 0., 1.);
+vec4 gray = vec4(.88);
 
 if(UV.x < xPartition && UV.y > yPartition) {
 // THREADING
@@ -10,19 +10,28 @@ if(UV.x < xPartition && UV.y > yPartition) {
 	vec2 luv = vec2(tuv.x / xPartition, tuv.y * (1. / (1. - yPartition)));
 	float v = step(abs(luv.y - th(luv.x)), .1);
 	v = dit(I, v);
-	FOut = mix(gray, blue, v);
+	FOut = mix(gray, black, v);
 } else if(UV.x > xPartition && UV.y < yPartition) {
 // TREADLING
 	vec2 tuv = vec2(UV.x - xPartition, UV.y);
 	vec2 luv = vec2(tuv.x * (1. / (1. - xPartition)), tuv.y / yPartition);
 	float v = step(abs(tr(luv.y) - luv.x), .1);
 	v = dit(I, v);
-	FOut = mix(gray, blue, v);
+	FOut = mix(gray, black, v);
 } else if(UV.x > xPartition && UV.y > yPartition) {
 // TIEUP
 	vec2 tuv = vec2(UV.x - xPartition, UV.y - yPartition);
 	vec2 luv = vec2(tuv.x * (1. / (1. - xPartition)), tuv.y * (1. / (1. - yPartition)));
 	float v = tie(luv);
+
+	// slight vibration/scanline/refresh effect
+	v += hash(
+			ivec3(
+				I, 
+				(time/10. * float(I.y)/200.) * 6.
+			)
+		).r * .18;
+	
 	v = dit(I, v);
 	FOut = vec4((1. - v) * gray);
 } else {
